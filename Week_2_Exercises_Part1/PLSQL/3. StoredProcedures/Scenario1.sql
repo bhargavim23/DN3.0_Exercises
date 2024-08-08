d591f69b481b59d75460f777c89ef6fef8635d12
bhargavim23 <115788108+bhargavim23@users.noreply.github.com>
2024-08-08 07:58:55 +0530

    Delete Week_2_Exercises_Part1

--- Week_2_Exercises_Part1/PLSQL/3. StoredProcedures/Scenario1.sql
+++
@@ -1,8 +0,0 @@
-DELIMITER //
-CREATE PROCEDURE ProcessMonthlyInterest()
-BEGIN
-    UPDATE Accounts SET Balance = Balance*1.01,
-    LastModified = CURRENT_DATE
-    WHERE AccountType = 'Savings';
-END
-//
